78b8cfb54451504c1c1db513d21128fb50899ac0
Kraken536 <abd.aa536@gmail.com>
2023-02-17 01:25:42 +0300

    what we've learned today.

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -12,7 +13,41 @@ export class RoomsComponent implements OnInit {
   texte = "font-weight: bold";
   hideRooms = false;
 
-  constructor(){
+  rooms: Room = {
+    availableRooms: 17,
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: "Deluxe Room",
+      amenities: "Free Wi-Fi, TV, Bathroom, Kitchen",
+      price: 500,
+      photos: "",
+      checkInTime: new Date('11-Nov-2021'),
+      checkOutTime: new Date('12-Nov-2021'),
+    },
+    {
+      roomNumber: 2,
+      roomType: "Private Suit",
+      amenities: "Free Wi-Fi, TV, Bathroom, Kitchen",
+      price: 1000,
+      photos: "",
+      checkInTime: new Date('14-Nov-2021'),
+      checkOutTime: new Date('17-Nov-2021'),
+    },
+    {
+      roomNumber: 3,
+      roomType: "Luxurious Room",
+      amenities: "Free Wi-Fi, TV, Bathroom, Kitchen",
+      price: 5000,
+      photos: "",
+      checkInTime: new Date('14-Nov-2021'),
+      checkOutTime: new Date('17-Nov-2021'),
+    }
+  ];
+
+  constructor() {
 
   };
 
@@ -20,7 +55,7 @@ export class RoomsComponent implements OnInit {
 
   };
 
-  toggle(){
+  toggle() {
     this.hideRooms = !this.hideRooms;
   }
 }
